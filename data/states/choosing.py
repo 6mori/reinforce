e228b4da299e4062b914bb3db029b9b4430a5a21
6mori <luhs1043@gmail.com>
2018-07-02 22:20:40 +0800

    change implement of choosing.

--- data/states/choosing.py
+++ data/states/choosing.py
@@ -27,13 +27,13 @@ class Choosing(tools._State):
 
 
     def setup_cursor(self):
-        self.cursor_1 = self.creat_cursor(c.P1_CHOOSE_BASE, 50, 50)
-        self.cursor_1.offset = (0, 0)
-        self.cursor_1.confirm = False
+        #self.cursor_1 = self.creat_cursor(c.P1_CHOOSE_BASE, 50, 50)
+        self.p1_offset = (0, 0)
+        self.p1_confirm = False
 
-        self.cursor_2 = self.creat_cursor(c.P2_CHOOSE_BASE, 50, 50)
-        self.cursor_2.offset = (0, 0)
-        self.cursor_2.confirm = False
+        #self.cursor_2 = self.creat_cursor(c.P2_CHOOSE_BASE, 50, 50)
+        self.p2_offset = (0, 0)
+        self.p2_confirm = False
 
 
     def setup_position(self):
@@ -62,20 +62,20 @@ class Choosing(tools._State):
                 pg.transform.scale(pg.image.load('images/posters/%s.png' % (character_name)), c.HALF_SCREEN_SIZE)
 
 
-    def creat_cursor(self, pos, height, width):
+    '''def creat_cursor(self, pos, height, width):
         cursor = pg.sprite.Sprite()
         cursor.image = pg.Surface([height, width])
         # self.cursor.image.set_colorkey(c.BLACK)
         cursor.rect = cursor.image.get_rect()
         cursor.rect.x = pos[0]
         cursor.rect.y = pos[1]
-        return cursor
+        return cursor'''
 
 
     def update(self, surface, keys, current_time):
         self.current_time = current_time
         self.game_info[c.CURRENT_TIME] = self.current_time
-        self.update_cursor()
+        #self.update_cursor()
         self.blit_everything(surface)
         self.check_if_all_confirmed()
 
@@ -97,52 +97,54 @@ class Choosing(tools._State):
         }
 
         if event.type == pg.KEYDOWN:
-            if self.cursor_2.confirm == False:
-                if event.key in P2_input_list.keys():
-                    command = P2_input_list[event.key]
-                    if command == c.CONFIRM:
-                        self.cursor_2.confirm = True
-                    else:
-                        next_pos = tuple(
-                            [cur + off for cur, off in zip(self.cursor_2.offset, tools.direct2pos[command])])
-                        if next_pos in self.pos2Chara.keys():
-                            self.cursor_2.offset = next_pos
-
-            elif self.cursor_1.confirm == False:
+            if self.p1_confirm == False:
                 if event.key in P1_input_list.keys():
                     command = P1_input_list[event.key]
                     if command == c.CONFIRM:
-                        self.cursor_1.confirm = True
+                        self.p1_confirm = True
                     else:
                         next_pos = tuple(
-                            [cur + off for cur, off in zip(self.cursor_1.offset, tools.direct2pos[command])])
+                            [cur + off for cur, off in zip(self.p1_offset, tools.direct2pos[command])])
                         if next_pos in self.pos2Chara.keys():
-                            self.cursor_1.offset = next_pos
+                            self.p1_offset = next_pos
+
+            if self.p2_confirm == False:
+                if event.key in P2_input_list.keys():
+                    command = P2_input_list[event.key]
+                    if command == c.CONFIRM:
+                        self.p2_confirm = True
+                    else:
+                        next_pos = tuple(
+                            [cur + off for cur, off in zip(self.p2_offset, tools.direct2pos[command])])
+                        if next_pos in self.pos2Chara.keys():
+                            self.p2_offset = next_pos
 
 
-    def update_cursor(self):
+    '''def update_cursor(self):
         self.cursor_1.rect.x, self.cursor_1.rect.y = [base + off for base, off in
                                                       zip(c.P1_CHOOSE_BASE, self.cursor_1.offset)]
         self.cursor_2.rect.x, self.cursor_2.rect.y = [base + off for base, off in
-                                                      zip(c.P2_CHOOSE_BASE, self.cursor_2.offset)]
+                                                      zip(c.P2_CHOOSE_BASE, self.cursor_2.offset)]'''
 
 
     def blit_everything(self, surface):
         surface.blit(self.background, self.background_rect)
-        surface.blit(self.chara_poster[self.pos2Chara[self.cursor_1.offset]], pg.Rect((0, 0), c.HALF_SCREEN_SIZE))
-        surface.blit(self.chara_poster[self.pos2Chara[self.cursor_2.offset]],
+        surface.blit(self.chara_poster[self.pos2Chara[self.p1_offset]], pg.Rect((0, 0), c.HALF_SCREEN_SIZE))
+        surface.blit(self.chara_poster[self.pos2Chara[self.p2_offset]],
                      pg.Rect((c.SCREEN_WIDTH / 2, 0), c.HALF_SCREEN_SIZE))
 
         for pos, chara in self.pos2Chara.items():
             surface.blit(self.chara_icon[chara][1], (c.P1_CHOOSE_BASE[0] + pos[0], c.P1_CHOOSE_BASE[1] + pos[1]))
             surface.blit(self.chara_icon[chara][1], (c.P2_CHOOSE_BASE[0] + pos[0], c.P2_CHOOSE_BASE[1] + pos[1]))
 
-        surface.blit(self.chara_icon[self.pos2Chara[self.cursor_1.offset]][0], self.cursor_1.rect)
-        surface.blit(self.chara_icon[self.pos2Chara[self.cursor_2.offset]][0], self.cursor_2.rect)
+        surface.blit(self.chara_icon[self.pos2Chara[self.p1_offset]][0],
+                     [base + off for base, off in zip(c.P1_CHOOSE_BASE, self.p1_offset)])
+        surface.blit(self.chara_icon[self.pos2Chara[self.p2_offset]][0],
+                     [base + off for base, off in zip(c.P2_CHOOSE_BASE, self.p2_offset)])
 
 
     def check_if_all_confirmed(self):
-        if self.cursor_1.confirm and self.cursor_2.confirm:
-            self.game_info[c.P1_CHARACTER] = self.pos2Chara[self.cursor_1.offset]
-            self.game_info[c.P2_CHARACTER] = self.pos2Chara[self.cursor_2.offset]
+        if self.p1_confirm and self.p2_confirm:
+            self.game_info[c.P1_CHARACTER] = self.pos2Chara[self.p1_offset]
+            self.game_info[c.P2_CHARACTER] = self.pos2Chara[self.p2_offset]
             self.done = True
